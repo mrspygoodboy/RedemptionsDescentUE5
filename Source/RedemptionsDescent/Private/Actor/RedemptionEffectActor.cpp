00b7bce435ff7727a5abf922074da03f2ca8cd55
mrspygoodboy <aliyansaleem2013@gmail.com>
2025-12-24 07:02:35 +0200

    Stripped the effect actor naked to move designer related stuff to blueprints

--- Source/RedemptionsDescent/Private/Actor/RedemptionEffectActor.cpp
+++ Source/RedemptionsDescent/Private/Actor/RedemptionEffectActor.cpp
@@ -3,50 +3,18 @@
 
 #include "Actor/RedemptionEffectActor.h"
 
-#include "AbilitySystemComponent.h"
-#include "AbilitySystemInterface.h"
-#include "AbilitySystem/RedemptionAttributeSet.h"
-#include "Components/SphereComponent.h"
 
 ARedemptionEffectActor::ARedemptionEffectActor()
 {
 	PrimaryActorTick.bCanEverTick = false;
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
-	SetRootComponent(Mesh);
-	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
-	
-	Sphere->SetupAttachment(GetRootComponent());
+	SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
 }
 
-void ARedemptionEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-{
-	// TODO: Change this to apply a GameplayEffect, for now using const_cast
-	 if (const IAbilitySystemInterface* ASCInterface = Cast<IAbilitySystemInterface>(OtherActor))
-	 {
-		 const URedemptionAttributeSet* RedemptionAttributeSet = Cast<URedemptionAttributeSet>
-	 	(ASCInterface->GetAbilitySystemComponent()->GetAttributeSet(URedemptionAttributeSet::StaticClass()));
-	 	
-	 	URedemptionAttributeSet* MutableAuraAttributeSet = const_cast<URedemptionAttributeSet*>(RedemptionAttributeSet);
-	 	MutableAuraAttributeSet->SetHealth(RedemptionAttributeSet->GetMana() + 25.f);
-	 	MutableAuraAttributeSet->SetMana(RedemptionAttributeSet->GetMana() - 25.f);
-	 	Destroy();
-	 }
-	 	
-}
-
-void ARedemptionEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-	
-}
 
 void ARedemptionEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
-	
-	Sphere->OnComponentBeginOverlap.AddDynamic(this, &ARedemptionEffectActor::OnOverlap);
-	Sphere->OnComponentEndOverlap.AddDynamic(this, &ARedemptionEffectActor::EndOverlap);
+
 }
 
 
--- Source/RedemptionsDescent/Public/Actor/RedemptionEffectActor.h
+++ Source/RedemptionsDescent/Public/Actor/RedemptionEffectActor.h
@@ -6,7 +6,7 @@
 #include "GameFramework/Actor.h"
 #include "RedemptionEffectActor.generated.h"
 
-class USphereComponent;
+class UGameplayEffect;
 
 UCLASS()
 class REDEMPTIONSDESCENT_API ARedemptionEffectActor : public AActor
@@ -15,21 +15,11 @@ class REDEMPTIONSDESCENT_API ARedemptionEffectActor : public AActor
 	
 public:	
 	ARedemptionEffectActor();
-	
-	UFUNCTION()
-	virtual void OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
-	UFUNCTION()
-	virtual void EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 protected:
 	virtual void BeginPlay() override;
-
-private:
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<USphereComponent> Sphere;
 	
-	UPROPERTY(VisibleAnywhere)
-	TObjectPtr<UStaticMeshComponent> Mesh;
-	
+	UPROPERTY(EditAnywhere, Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
 };
